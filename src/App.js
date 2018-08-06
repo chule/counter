5ddc3c5c6de89b35edf3026ea17aa0cbc89561f5
Igor Cuckovic <cuckovic@gmail.com>
2018-08-06 22:04:08 +0200

    authentication

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { auth, database, googleAuthProvider } from './firebase';
 
 class App extends Component {
 
   state = {
-    counter: 0
+    counter: 0,
+    user: null
   }
 
   addOne = () => {
@@ -20,13 +22,56 @@ class App extends Component {
     }))
   }
 
+  signOut = (e) => {
+    if (e) {
+      e.preventDefault();
+    }
+    auth.signOut().then(() => {
+      this.setState(() => ({
+        user: null
+      }))
+    })
+    this.reset()
+  }
+
+  signIn = (e) => {
+    if (e) {
+      e.preventDefault()
+    }
+
+
+
+    auth.signInWithPopup(googleAuthProvider)
+      .then(result => {
+        console.log(result)
+        if (result.user) {
+          this.setState(() => ({
+            user: result.user
+          }))
+        }
+      })
+
+  }
+
 
   render() {
+    const { user } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Counter</h1>
+          {
+            user
+              ? <button onClick={this.signOut}>
+                {"Sign Out " + user.displayName.split(" ")[0]}
+              </button>
+
+              : <button onClick={this.signIn}>
+                Sign in
+              </button>
+          }
         </header>
         <p className="App-intro">
           {this.state.counter}
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBOkmm4DOULm2wKReyuM0cIlITGuRt9wP8",
+    authDomain: "cou-nter.firebaseapp.com",
+    databaseURL: "https://cou-nter.firebaseio.com",
+    projectId: "cou-nter",
+    storageBucket: "cou-nter.appspot.com",
+    messagingSenderId: "393777055254"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
