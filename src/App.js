d7ea7c18b5f235fa6504ffda89547f769786e7d8
Igor Cuckovic <cuckovic@gmail.com>
2018-08-06 21:21:50 +0200

    start

--- src/App.js
+++ src/App.js
@@ -3,16 +3,39 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    counter: 0
+  }
+
+  addOne = () => {
+    this.setState(oldState => ({
+      counter: oldState.counter + 1
+    }))
+  }
+
+  reset = () => {
+    this.setState(() => ({
+      counter: 0
+    }))
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Counter</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          {this.state.counter}
         </p>
+
+        <button className="Add-button" onClick={this.addOne}>Add one</button>
+        <br />
+        <button onClick={this.reset}>Reset</button>
+
       </div>
     );
   }
